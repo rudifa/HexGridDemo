45cafb42a0deaa02773396444987e61f6d3651fb
Rudi Farkas <rudi.farkas@gmail.com>
2021-11-26 14:00:15 +0100

    Initial Commit

---
+++ Shared/HexGridDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HexGridDemoApp.swift
+//  Shared
+//
+//  Created by Rudolf Farkas on 26.11.21.
+//
+
+import SwiftUI
+
+@main
+struct HexGridDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
